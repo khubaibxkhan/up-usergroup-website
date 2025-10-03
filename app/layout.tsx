7e03814bde1e51e5a3d7c5f7afc3d94bc8c275ba
khubaixkhan <khubaibxkhan22@gmail.com>
2025-10-03 12:18:35 +0530

    fixing deployment errors by removing my empty header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+
 
 export const metadata: Metadata = {
   title: "Microsoft User Group Power BI Club - Uttar Pradesh",
@@ -17,9 +16,7 @@ export default function RootLayout({
       <html lang="en">
       <body className="font-poppins antialiased">
         <div className="flex flex-col min-h-screen">
-          <Header />
           <main className="flex-1">{children}</main>
-          <Footer />
         </div>
         </body>
     </html>
